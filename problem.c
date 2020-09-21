a4259aab3a860104d6f957d721ba5a2d4fde67d1
Timir <islam.timir772@gmail.com>
2020-09-21 15:28:40 +0600

    solved 1149

--- problem.c
+++ problem.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
 int main () {
+    int a,n,i,result = 0,temp = 0,sum = 0;
+    scanf("%d %d", &a, &n);
+    while (1) {
+        if ( n <= 0 ) {
+            scanf("%d", &n);
+            continue;
+        }
+        break;
+    }
+    for ( i = 1; i <= n; i++) {
+        result = a++;
+        sum+=result;
+    }
+    printf("%d", sum);
     
-
-
-
+    
+    
     return 0;
 }
